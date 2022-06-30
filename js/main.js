e343ea6a098a76740110a7d7c0633ea4f8ef5d46
Bae Sujeong <doya60@gmail.com>
2022-06-30 16:04:45 +0900

    스크롤 모션 추가

--- js/main.js
+++ js/main.js
@@ -6,7 +6,20 @@ for (let section of mainSecs) {
 }
 
 window.addEventListener("scroll", () => {
+  const aboutSec = document.querySelector("#about");
   const proSec = document.querySelector("#project");
+
+  if (this.scrollY > secPosArr[1] - 400) {
+    aboutSec.querySelector(".contact-wrap").classList.add("on");
+  } else {
+    aboutSec.querySelector(".contact-wrap").classList.remove("on");
+  }
+
+  if (this.scrollY > secPosArr[1] - 200) {
+    aboutSec.querySelector(".info-wrap").classList.add("on");
+  } else {
+    aboutSec.querySelector(".info-wrap").classList.remove("on");
+  }
 
   if (this.scrollY > secPosArr[2] - 200) {
     proSec.querySelector(".cont1").classList.add("on");
@@ -14,7 +27,7 @@ window.addEventListener("scroll", () => {
     proSec.querySelector(".cont1").classList.remove("on");
   }
 
-  if (this.scrollY > secPosArr[2] + 100) {
+  if (this.scrollY > secPosArr[2] + 200) {
     proSec.querySelector(".cont2").classList.add("on");
   } else {
     proSec.querySelector(".cont2").classList.remove("on");
